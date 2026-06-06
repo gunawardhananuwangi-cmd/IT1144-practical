961a8131ecc0bc7295f520a99d4330f90532a7d9
gunawardhananuwangi-cmd <gunawardhananuwangi@gmail.com>
2026-06-06 20:35:45 +0530

    Add files via upload

---
+++ Switch.js
@@ -0,0 +1,15 @@
+let fruit="Apple";
+	switch(fruit){
+	case"apple":
+		console.log("It is an apple");
+		break;
+	case "Apple":
+		console.log("It is an Apple");
+		break;
+	case"banana":
+		console.log("It is a banana");
+		break;
+	default:
+		console.log("It is not a fruit");
+}
+	

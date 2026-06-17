e46dede39f7b7ac2f28b1afab1e721d8c355f5a9
gunawardhananuwangi-cmd <gunawardhananuwangi@gmail.com>
2026-06-17 06:39:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function goToPlants() {
+    document.querySelector('.btn-main');
+    
+    setTimeout(function() {
+        window.location.href = "Plants.html";
+    }, 200);
+}
+
+function addToCart(){
+    document.querySelector('.btn-main');
+    
+    setTimeout(function() {
+        window.location.href = "contact.html";
+    }, 200);
+}
+
+function buyMassage(){
+	alert("You can get 20% offer.🎉");
+}
